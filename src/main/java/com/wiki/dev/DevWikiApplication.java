03a58b5c462370833979efb036f46b84bb0c237d
sdy <vel1024@khu.ac.kr>
2021-03-26 17:37:33 +0900

    create main

---
+++ src/main/java/com/wiki/dev/DevWikiApplication.java
@@ -0,0 +1,13 @@
+package com.wiki.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWikiApplication.class, args);
+	}
+
+}
