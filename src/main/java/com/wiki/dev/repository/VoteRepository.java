ac5e0a80684e78b5a6f9dc851ba24bf905af7b42
sdy <vel1024@khu.ac.kr>
2021-03-26 17:39:34 +0900

    create Vote Repository

---
+++ src/main/java/com/wiki/dev/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.wiki.dev.repository;
+
+import com.wiki.dev.entity.Post;
+import com.wiki.dev.entity.User;
+import com.wiki.dev.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+}
