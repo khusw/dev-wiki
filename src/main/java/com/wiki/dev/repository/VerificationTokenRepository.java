d40de61be0adcbe783c3476d3eff8b5b20bbabd1
sdy <vel1024@khu.ac.kr>
2021-03-26 17:39:24 +0900

    create VerificationToken Repository

---
+++ src/main/java/com/wiki/dev/repository/VerificationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.wiki.dev.repository;
+
+import com.wiki.dev.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
