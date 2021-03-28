354038bacc91ea5bc902b9e7d70dfca627f526ef
sdy <vel1024@khu.ac.kr>
2021-03-29 00:05:32 +0900

    create AuthService

---
+++ src/main/java/com/wiki/dev/service/AuthService.java
@@ -0,0 +1,52 @@
+package com.wiki.dev.service;
+
+import com.wiki.dev.dto.RegisterRequest;
+import com.wiki.dev.entity.User;
+import com.wiki.dev.entity.VerificationToken;
+import com.wiki.dev.repository.UserRepository;
+import com.wiki.dev.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
