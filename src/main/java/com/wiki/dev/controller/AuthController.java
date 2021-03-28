46164f38ec0046a3fd0c0181a1bb5fa401521726
sdy <vel1024@khu.ac.kr>
2021-03-29 00:05:46 +0900

    create AuthController

---
+++ src/main/java/com/wiki/dev/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.wiki.dev.controller;
+
+import com.wiki.dev.dto.RegisterRequest;
+import com.wiki.dev.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rest")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
