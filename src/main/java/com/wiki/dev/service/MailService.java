8d9e2bf2a5209f31c41bf572a38a406ecfd39260
sdy <vel1024@khu.ac.kr>
2021-03-31 16:58:16 +0900

    create MailService.java

---
+++ src/main/java/com/wiki/dev/service/MailService.java
@@ -0,0 +1,39 @@
+package com.wiki.dev.service;
+
+import com.wiki.dev.entity.NotificationEmail;
+import com.wiki.dev.exception.DevWikiException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    @Async
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("dev-wiki@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+            log.info("activation email sent");
+        } catch (MailException e) {
+            throw new DevWikiException("Exception occurred when sending email to " + notificationEmail.getRecipient());
+        }
+    }
+}
