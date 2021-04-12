dac711b3ac9b08dce736eae52f3c83f20f4e2e05
sdy <vel1024@khu.ac.kr>
2021-04-12 22:46:16 +0900

    create CommentMapper.java

---
+++ src/main/java/com/wiki/dev/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.wiki.dev.mapper;
+
+import com.wiki.dev.dto.CommentsDto;
+import com.wiki.dev.entity.Comment;
+import com.wiki.dev.entity.Post;
+import com.wiki.dev.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
