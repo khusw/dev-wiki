c3b1c8bda17f2abb99fa2c3356ffaea5a93b3e79
sdy <vel1024@khu.ac.kr>
2021-04-15 21:02:01 +0900

    update PostMapper

--- src/main/java/com/wiki/dev/mapper/PostMapper.java
+++ src/main/java/com/wiki/dev/mapper/PostMapper.java
@@ -1,22 +1,29 @@
 package com.wiki.dev.mapper;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.wiki.dev.dto.PostRequest;
 import com.wiki.dev.dto.PostResponse;
 import com.wiki.dev.entity.Category;
 import com.wiki.dev.entity.Post;
 import com.wiki.dev.entity.User;
+import com.wiki.dev.repository.CommentRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "postName", source = "postRequest.postName")
     @Mapping(target = "category", source = "category")
     @Mapping(target = "user", source = "user")
     @Mapping(target = "description", source = "postRequest.description")
-    Post map(PostRequest postRequest, Category category, User user);
+    @Mapping(target = "voteCount", constant = "0")
+    public abstract Post map(PostRequest postRequest, Category category, User user);
 
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "postName", source = "postName")
@@ -24,5 +31,15 @@ public interface PostMapper {
     @Mapping(target = "url", source = "url")
     @Mapping(target = "categoryName", source = "category.name")
     @Mapping(target = "userName", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
