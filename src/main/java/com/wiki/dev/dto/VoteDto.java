c0bef7da66497e20dbd0280aa01550d597f978a8
sdy <vel1024@khu.ac.kr>
2021-04-15 21:00:56 +0900

    create VoteDto

---
+++ src/main/java/com/wiki/dev/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.wiki.dev.dto;
+
+import com.wiki.dev.entity.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
