a8113b38f8306c480d2dee30a3bee559ad63d046
sdy <vel1024@khu.ac.kr>
2021-04-16 15:44:08 +0900

    add two method (logout, refreshToken)

--- src/main/java/com/wiki/dev/controller/AuthController.java
+++ src/main/java/com/wiki/dev/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.wiki.dev.controller;
 
 import com.wiki.dev.dto.AuthenticationResponse;
 import com.wiki.dev.dto.LoginRequest;
+import com.wiki.dev.dto.RefreshTokenRequest;
 import com.wiki.dev.dto.RegisterRequest;
 import com.wiki.dev.service.AuthService;
+import com.wiki.dev.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,11 +19,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>("User registration successful",HttpStatus.OK);
     }
 
     @GetMapping("/accountVerification/{token}")
@@ -33,4 +38,14 @@ public class AuthController {
         return authService.login(loginRequest);
     }
 
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(HttpStatus.OK).body("Refresh Token Deleted Successfully ! ");
+    }
 }
