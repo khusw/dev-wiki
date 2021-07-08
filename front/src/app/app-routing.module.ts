d838f7eac1ec2128022819e4677bd3e374a71702
sdy <vel1024@khu.ac.kr>
2021-07-08 14:19:39 +0900

    update global routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from "./auth/login/login.component";
 import { HomeComponent } from "./home/home.component";
+import {CreatePostComponent} from "./post/create-post/create-post.component";
+import {CreateCategoryComponent} from "./category/create-category/create-category.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-category', component: CreateCategoryComponent }
 ];
 
 @NgModule({
