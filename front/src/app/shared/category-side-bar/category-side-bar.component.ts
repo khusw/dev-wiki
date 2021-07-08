67466c66604f4d33430caa636b589bf23a3a3e91
sdy <vel1024@khu.ac.kr>
2021-07-08 14:15:57 +0900

    create category-side-bar component

---
+++ front/src/app/shared/category-side-bar/category-side-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {CategoryService} from "../../category/category.service";
+
+@Component({
+  selector: 'app-category-side-bar',
+  templateUrl: './category-side-bar.component.html',
+  styleUrls: ['./category-side-bar.component.css']
+})
+export class CategorySideBarComponent implements OnInit {
+
+  categories: Array<any> = [];
+  displayViewAll: boolean;
+
+  constructor(private categoryService: CategoryService) {
+    this.categoryService.getAllCategories().subscribe(data => {
+      if (data.length > 3) {
+        this.categories = data.splice(0, 3);
+        this.displayViewAll = true;
+      } else {
+        this.categories = data;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
