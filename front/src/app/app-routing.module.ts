1ca3ebc4799f2e0091a9ad210ae12f14c7e9579a
sdy <vel1024@khu.ac.kr>
2021-07-12 20:07:21 +0900

    update routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,19 +3,25 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from "./auth/login/login.component";
 import { HomeComponent } from "./home/home.component";
-import {CreatePostComponent} from "./post/create-post/create-post.component";
-import {CreateCategoryComponent} from "./category/create-category/create-category.component";
+import { CreatePostComponent } from "./post/create-post/create-post.component";
+import { CreateCategoryComponent } from "./category/create-category/create-category.component";
+import { ListCategoriesComponent } from "./category/list-categories/list-categories.component";
+import { ViewPostComponent } from "./post/view-post/view-post.component";
+import {UserProfileComponent} from "./auth/user-profile/user-profile.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'create-post', component: CreatePostComponent },
-  { path: 'create-category', component: CreateCategoryComponent }
+  { path: "", component: HomeComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "create-post", component: CreatePostComponent, canActivate: [AuthGuard] },
+  { path: "create-category", component: CreateCategoryComponent, canActivate: [AuthGuard] },
+  { path: "list-categories", component: ListCategoriesComponent },
+  { path: "view-post/:id", component: ViewPostComponent },
+  { path: "user-profile/:name", component: UserProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
-  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
