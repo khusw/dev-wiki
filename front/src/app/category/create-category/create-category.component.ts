73bedb9008eb94dfb0c8211704cae6eb2376de6c
sdy <vel1024@khu.ac.kr>
2021-07-12 20:05:12 +0900

    create create-category component

--- front/src/app/category/create-category/create-category.component.ts
+++ front/src/app/category/create-category/create-category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { CategoryModel } from "../category-model";
+import {Router} from "@angular/router";
+import {CategoryService} from "../category.service";
 
 @Component({
   selector: 'app-create-category',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCategoryComponent implements OnInit {
 
-  constructor() { }
+  createCategoryForm: FormGroup;
+  categoryModel: CategoryModel;
+  title = new FormControl("");
+  description = new FormControl("");
+
+  constructor(private router: Router, private categoryService: CategoryService) {
+    this.createCategoryForm = new FormGroup({
+      title: new FormControl("", Validators.required),
+      description: new FormControl("", Validators.required)
+    });
+
+    this.categoryModel = {
+      name: "",
+      description: ""
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  discard() {
+    this.router.navigateByUrl("/");
+  }
+
+  createCategory() {
+    this.categoryModel.name = this.createCategoryForm.get("title").value;
+    this.categoryModel.description = this.createCategoryForm.get("description").value;
+    this.categoryService.createCategory(this.categoryModel).subscribe(data => {
+      this.router.navigateByUrl("/list-categories");
+    }, error => {
+      console.log("Error occured : " + error);
+    });
+  }
 }
