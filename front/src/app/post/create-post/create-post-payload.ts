9b2c4e1507acab8facb7541d8276674abcd2f912
sdy <vel1024@khu.ac.kr>
2021-07-12 20:05:51 +0900

    create create-post component

---
+++ front/src/app/post/create-post/create-post-payload.ts
@@ -0,0 +1,6 @@
+export class CreatePostPayload {
+  postName: string;
+  categoryName?: string;
+  url?: string;
+  description: string;
+}
--- front/src/app/post/create-post/create-post.component.ts
+++ front/src/app/post/create-post/create-post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CreatePostPayload} from "./create-post-payload";
+import {Router} from "@angular/router";
+import {PostService} from "../post.service";
+import {CategoryService} from "../../category/category.service";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-create-post',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  createPostForm: FormGroup;
+  postPayload: CreatePostPayload;
+  categories: Array<any>;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private postService: PostService, private categoryService: CategoryService) {
+    this.postPayload = {
+      postName: "",
+      url: "",
+      description: "",
+      categoryName: ""
+    }
   }
 
+  ngOnInit(): void {
+    this.createPostForm = new FormGroup({
+      postName: new FormControl("", Validators.required),
+      categoryName: new FormControl("", Validators.required),
+      url: new FormControl("", Validators.required),
+      description: new FormControl("", Validators.required)
+    });
+
+    this.categoryService.getAllCategories().subscribe(data => {
+      this.categories = data;
+    }, error => {
+      throwError(error);
+    });
+  }
+
+  createPost() {
+    this.postPayload.postName = this.createPostForm.get("postName").value;
+    this.postPayload.categoryName = this.createPostForm.get("categoryName").value;
+    this.postPayload.url = this.createPostForm.get("url").value;
+    this.postPayload.description = this.createPostForm.get("description").value;
+
+    this.postService.createPost(this.postPayload).subscribe(data => {
+      this.router.navigateByUrl("/");
+    }, error => {
+      throwError(error);
+    });
+  }
+
+  discardPost() {
+    this.router.navigateByUrl("/");
+  }
 }
