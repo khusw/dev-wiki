10e0084e2785c09939dfc59a720c62b17a3f5a15
sdy <vel1024@khu.ac.kr>
2021-07-23 22:51:42 +0900

    add validation code

--- src/main/java/com/wiki/dev/dto/CommentsDto.java
+++ src/main/java/com/wiki/dev/dto/CommentsDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 
 @Data
@@ -13,6 +14,8 @@ public class CommentsDto {
     private Long commentId;
     private Long postId;
     private Instant createdDate;
+
+    @NotEmpty(message = "comment's text is required !")
     private String text;
     private String userName;
 }
