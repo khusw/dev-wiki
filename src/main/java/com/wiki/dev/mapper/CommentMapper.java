45c6ff4ab88942a4d4fa51d3fc7da0e7d8a744b2
sdy <vel1024@khu.ac.kr>
2021-07-23 22:51:29 +0900

    rename comment mapper functions

--- src/main/java/com/wiki/dev/mapper/CommentMapper.java
+++ src/main/java/com/wiki/dev/mapper/CommentMapper.java
@@ -14,9 +14,9 @@ public interface CommentMapper {
     @Mapping(target = "text", source = "commentsDto.text")
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "post", source = "post")
-    Comment map(CommentsDto commentsDto, Post post, User user);
+    Comment mapDtoToComment(CommentsDto commentsDto, Post post, User user);
 
     @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
     @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
-    CommentsDto mapToDto(Comment comment);
+    CommentsDto mapCommentToDto(Comment comment);
 }
