fd20aa5f50358ba6d2de7026df3bda57ba6310fc
sdy <vel1024@khu.ac.kr>
2021-07-24 23:19:21 +0900

    update category data

--- front/src/app/shared/category-side-bar/category-side-bar.component.ts
+++ front/src/app/shared/category-side-bar/category-side-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from "../../category/category.service";
+import {CategoryModel} from "../../category/category-model";
 
 @Component({
   selector: 'app-category-side-bar',
@@ -8,16 +9,16 @@ import {CategoryService} from "../../category/category.service";
 })
 export class CategorySideBarComponent implements OnInit {
 
-  categories: Array<any> = [];
+  categories: Array<CategoryModel> = [];
   displayViewAll: boolean;
 
   constructor(private categoryService: CategoryService) {
     this.categoryService.getAllCategories().subscribe(data => {
-      if (data.length > 3) {
-        this.categories = data.splice(0, 3);
+      if (data['data'].length > 3) {
+        this.categories = data['data'].splice(0, 3);
         this.displayViewAll = true;
       } else {
-        this.categories = data;
+        this.categories = data['data'];
       }
     });
   }
