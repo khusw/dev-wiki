fd29c7b4a75c7bdba6a1564ae00064fa88f247c5
sdy <vel1024@khu.ac.kr>
2021-08-10 23:34:49 +0900

    update devtools scope, allowed url

--- src/main/java/com/wiki/dev/config/SecurityConfig.java
+++ src/main/java/com/wiki/dev/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/posts/**")
                 .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/comments/**")
+                .permitAll()
                 .antMatchers(
                         "/v2/api-docs",
                         "/configuration/ui",
